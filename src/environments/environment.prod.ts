50edaeeae0773cd349dac8ae1b49259d96c5a9b5
Dmitry Shashkov <dmitry.the.wise@gmail.com>
2019-08-11 12:05:47 +0300

    Post-update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
 };
 
-export const SELF_URL = 'http://localhost:4200';
+export const SELF_URL = 'https://dmitryshashkov.github.io/auto-magic';
 
 export const GOOGLE_CLIENT_ID = '214193160548-cfg1enuesqjb8tumncopr4ei0ne9ebsa.apps.googleusercontent.com';
