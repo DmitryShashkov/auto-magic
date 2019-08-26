13760e075d0433d0025f015a94e876baf3b18771
Dmitry Shashkov <dmitry.the.wise@gmail.com>
2019-08-26 22:19:15 +0300

    Colors and textures

--- src/app/components/shared/card/card.component.ts
+++ src/app/components/shared/card/card.component.ts
@@ -4,7 +4,7 @@ import { CardModel } from "../../../models/card.model";
 @Component({
     selector: 'am-card',
     templateUrl: './card.component.html',
-    styleUrls: ['./card.component.scss'],
+    styleUrls: ['./../../../../assets/styles/mana-colors.scss', './card.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
