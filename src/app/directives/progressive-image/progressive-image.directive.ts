eec165102f15eec5b20e3eb6f3d7faa0595fe99e
Dmitry Shashkov <dmitry.the.wise@gmail.com>
2019-08-28 23:42:33 +0300

    WIP: progressive image

--- src/app/directives/progressive-image/progressive-image.directive.ts
+++ src/app/directives/progressive-image/progressive-image.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, ElementRef, Input, OnChanges, AfterViewInit, Inject } from "@angular/core";
 import { DOCUMENT } from "@angular/common";
 import { PROGRESSIVE_IMAGE_LOADER_DATA } from "./progressive-image.loader-data";
+import { Subject, combineLatest } from "rxjs";
+import { first } from "rxjs/operators";
 
 @Directive({
     selector: '[progressiveImage]'
@@ -8,33 +10,75 @@ import { PROGRESSIVE_IMAGE_LOADER_DATA } from "./progressive-image.loader-data";
 export class ProgressiveImageDirective implements OnChanges, AfterViewInit {
     @Input() public width: number = 0;
     @Input() public height: number = 0;
-
-    private backgroundColor: string = '#eee';
+    @Input() public source: string;
 
     private get element (): HTMLElement {
         return this.elementRef.nativeElement;
     }
 
+    private sourceInitialized: Subject<boolean> = new Subject<boolean>();
+    private viewInitialized: Subject<boolean> = new Subject<boolean>();
+
+    private static get isNativeLazyLoadingSupported (): boolean {
+        return 'loading' in HTMLImageElement.prototype;
+    }
+
     constructor (
         private readonly elementRef: ElementRef<HTMLElement>,
         @Inject(DOCUMENT) private readonly document: Document,
-    ) { }
+    ) {
+        combineLatest(this.sourceInitialized, this.viewInitialized)
+            .pipe(first())
+            .subscribe(this.injectImage.bind(this));
+    }
 
     public ngOnChanges (): void {
         this.element.style.width = `${this.width}px`;
         this.element.style.height = `${this.height}px`;
+
+        if (this.source) {
+            this.sourceInitialized.next(true);
+        }
     }
 
     public ngAfterViewInit (): void {
-        this.element.style.backgroundColor = this.backgroundColor;
         this.element.style.position = 'relative';
 
         const spinnerLayer: HTMLDivElement = this.document.createElement('div');
-        spinnerLayer.style.width = '32px';
-        spinnerLayer.style.height = '32px';
-        spinnerLayer.style.backgroundImage = `url(${PROGRESSIVE_IMAGE_LOADER_DATA})`;
-        spinnerLayer.style.backgroundRepeat = 'no-repeat';
+
+        spinnerLayer.style.width = '40px';
+        spinnerLayer.style.height = '40px';
+        spinnerLayer.style.position = 'absolute';
+        spinnerLayer.style.top = '50%';
+        spinnerLayer.style.left  = '50%';
+        spinnerLayer.style.marginTop = '-20px';
+        spinnerLayer.style.marginLeft = '-20px';
+        spinnerLayer.style.animation = 'rotation 1.5s infinite linear';
+
+        spinnerLayer.innerHTML = PROGRESSIVE_IMAGE_LOADER_DATA;
 
         this.element.appendChild(spinnerLayer);
+
+        this.viewInitialized.next(true);
+    }
+
+    private injectImage (): void {
+        if (ProgressiveImageDirective.isNativeLazyLoadingSupported) {
+            const imageLayer: HTMLImageElement = this.document.createElement('img');
+            const imageLoadingAttr: Attr = this.document.createAttribute('loading');
+
+            imageLoadingAttr.value = 'lazy';
+
+            imageLayer.src = this.source;
+            imageLayer.attributes.setNamedItem(imageLoadingAttr);
+
+            imageLayer.style.width = '100%';
+            imageLayer.style.height = '100%';
+            imageLayer.style.objectFit = 'cover';
+            imageLayer.style.objectPosition = 'center';
+            imageLayer.style.position = 'relative';
+
+            this.element.appendChild(imageLayer);
+        }
     }
 }
