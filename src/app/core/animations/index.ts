7cf83c264a012d3f6ecc743116182d10e0cfd8b7
Dmitry Shashkov <dmitry.the.wise@gmail.com>
2019-09-15 12:34:42 +0300

    Hotfix: build

--- src/app/core/animations/index.ts
+++ src/app/core/animations/index.ts
@@ -1,20 +1,27 @@
-import { animate, animateChild, group, query as q, style, transition, trigger } from '@angular/animations';
-const query = (s, a, o= { optional:true }) => q(s, a, o);
+import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
 
 export const routerTransition = trigger('routerTransition', [
     transition('* => *', [
-        query(':enter, :leave', style({ position: 'fixed', width:'100%', height:'100%' })),
-        query(':enter', style({ transform: 'translateX(100%)' })),
+        query(':enter, :leave', style({ position: 'fixed', width:'100%', height:'100%' }), { optional: true }),
+        query(':enter', style({ transform: 'translateX(100%)' }), { optional: true }),
 
         group([
-            query(':leave', [
-                style({ transform: 'translateX(0%)' }),
-                animate('1.0s ease-in-out', style({ transform: 'translateX(-100%)' })),
-            ]),
-            query(':enter', [
-                animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
-                animateChild(),
-            ]),
+            query(
+                ':leave',
+                [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('1.0s ease-in-out', style({ transform: 'translateX(-100%)' })),
+                ],
+                { optional: true },
+            ),
+            query(
+                ':enter',
+                [
+                    animate('1.0s ease-in-out', style({ transform: 'translateX(0%)' })),
+                    animateChild(),
+                ],
+                { optional: true },
+            ),
         ]),
 
         query('router-outlet ~ *', [style({}), animate(1, style({}))], { optional: true }),
